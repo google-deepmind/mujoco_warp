1425b6651dae46de77736f626252306e170849c1
Baruch Tabanpour <btaba@google.com>
2025-04-29 13:19:36 -0700

    reorder.

--- mujoco_warp/_src/smooth.py
+++ mujoco_warp/_src/smooth.py
@@ -117,6 +117,20 @@ def kinematics(m: Model, d: Data):
     )
 
   @kernel
+  def mocap(m: Model, d: Data):
+    worldid, mocapid = wp.tid()
+    bodyid = m.mocap_bodyid[mocapid]
+    mocap_quat = wp.normalize(d.mocap_quat[worldid, mocapid])
+    xpos = d.mocap_pos[worldid, mocapid]
+    d.xpos[worldid, bodyid] = xpos
+    d.xquat[worldid, bodyid] = mocap_quat
+    d.xmat[worldid, bodyid] = math.quat_to_mat(mocap_quat)
+    d.xipos[worldid, bodyid] = xpos + math.rot_vec_quat(m.body_ipos[bodyid], mocap_quat)
+    d.ximat[worldid, bodyid] = math.quat_to_mat(
+      math.mul_quat(mocap_quat, m.body_iquat[bodyid])
+    )
+
+  @kernel
   def geom_local_to_global(m: Model, d: Data):
     worldid, geomid = wp.tid()
     bodyid = m.geom_bodyid[geomid]
@@ -136,20 +150,6 @@ def kinematics(m: Model, d: Data):
     d.site_xpos[worldid, siteid] = xpos + math.rot_vec_quat(m.site_pos[siteid], xquat)
     d.site_xmat[worldid, siteid] = math.quat_to_mat(
       math.mul_quat(xquat, m.site_quat[siteid])
-    )
-
-  @kernel
-  def mocap(m: Model, d: Data):
-    worldid, mocapid = wp.tid()
-    bodyid = m.mocap_bodyid[mocapid]
-    mocap_quat = wp.normalize(d.mocap_quat[worldid, mocapid])
-    xpos = d.mocap_pos[worldid, mocapid]
-    d.xpos[worldid, bodyid] = xpos
-    d.xquat[worldid, bodyid] = mocap_quat
-    d.xmat[worldid, bodyid] = math.quat_to_mat(mocap_quat)
-    d.xipos[worldid, bodyid] = xpos + math.rot_vec_quat(m.body_ipos[bodyid], mocap_quat)
-    d.ximat[worldid, bodyid] = math.quat_to_mat(
-      math.mul_quat(mocap_quat, m.body_iquat[bodyid])
     )
 
   wp.launch(_root, dim=(d.nworld), inputs=[m, d])
