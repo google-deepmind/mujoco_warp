a30bb0f0b53507a8d7856eafb366f5aa79f27c03
Alain Denzler <adenzler@nvidia.com>
2025-05-12 14:28:30 +0200

    small fix

--- mujoco_warp/_src/solver.py
+++ mujoco_warp/_src/solver.py
@@ -22,7 +22,6 @@ from . import types
 from .warp_util import event_scope
 from .warp_util import kernel as nested_kernel
 
-
 @wp.func
 def _rescale(nv: int, stat_meaninertia: float, value: float) -> float:
   return value / (stat_meaninertia * float(wp.max(1, nv)))
@@ -2013,6 +2012,7 @@ def update_gradient_JTDAJ(
   dof_tri_row: wp.array(dtype=int),
   dof_tri_col: wp.array(dtype=int),
   # Data in:
+  njmax_in: int,
   nefc_in: wp.array(dtype=int),
   efc_worldid_in: wp.array(dtype=int),
   efc_J_in: wp.array2d(dtype=float),
@@ -2033,7 +2033,7 @@ def update_gradient_JTDAJ(
   for i in range(nblocks_perblock):
     efcid = efcid_temp + i * dim_x
 
-    if efcid >= nefc:
+    if efcid >= min(nefc, njmax_in):
       return
 
     worldid = efc_worldid_in[efcid]
@@ -2256,6 +2256,7 @@ def _update_gradient(m: types.Model, d: types.Data):
       inputs=[
         m.dof_tri_row,
         m.dof_tri_col,
+        d.njmax,
         d.nefc,
         d.efc.worldid,
         d.efc.J,
