aca693ab8e9223ed13c7af15e2570b54c39739bf
Alain Denzler <adenzler@nvidia.com>
2025-06-12 09:34:35 +0200

    revert conftest changes
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>

--- conftest.py
+++ conftest.py
@@ -15,8 +15,6 @@
 
 import warp as wp
 
-collect_ignore = ["benchmark/mujoco_menagerie"]
-
 def pytest_addoption(parser):
   parser.addoption("--cpu", action="store_true", default=False, help="run tests with cpu")
   parser.addoption(
