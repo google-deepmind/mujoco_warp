2a475b080f3c4c4a96b40c070c1bb6d386b09cf3
Baruch Tabanpour <btaba@google.com>
2025-06-30 23:09:16 -0700

    simplify

--- mujoco_warp/_src/io.py
+++ mujoco_warp/_src/io.py
@@ -352,17 +352,15 @@ def put_model(mjm: mujoco.MjModel, _nworld: int = 1) -> types.Model:
   geom_type_pair = tuple(int(t) for tp in geom_type_pair for t in tp)
 
   def create_nmodel_batched_array(mjm_array, dtype, expand_dim=True):
-    nworld = max(_nworld, 1)
-
     def tile(x):
       ndim = len(x.shape) if expand_dim else len(x.shape) - 1
-      return np.tile(x, (nworld,) + (1,) * ndim)
+      return np.tile(x, (max(_nworld, 1),) + (1,) * ndim)
 
     array = wp.array(tile(mjm_array), dtype=dtype)
     if _nworld > 1:
       return array
 
-    array.strides = (0,) + array.strides
+    array.strides = (0,) + array.strides[1:]
     return array
 
   # rangefinder
