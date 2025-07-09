e1e075e71303750e6402ad02c31d3fbdcee2e889
Alain Denzler <adenzler@nvidia.com>
2025-07-09 10:28:03 +0200

    fix wrong import
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>

--- mujoco_warp/_src/warp_util.py
+++ mujoco_warp/_src/warp_util.py
@@ -19,7 +19,7 @@ from typing import Callable, Optional
 import warp as wp
 from warp.context import Module
 from warp.context import get_module
-from warp.runtime import assert_conditional_graph_support
+from warp.context import assert_conditional_graph_support
 from types import Model
 
 _STACK = None
