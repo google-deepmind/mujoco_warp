9e36f76bc6c345b6fb1942f51041449c29db169c
Alain Denzler <adenzler@nvidia.com>
2025-11-28 11:52:11 +0100

    fix padding calculation
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>

--- mujoco_warp/_src/solver.py
+++ mujoco_warp/_src/solver.py
@@ -1821,7 +1821,7 @@ def _update_gradient(m: types.Model, d: types.Data):
       )
 
     # TODO(team): Define good threshold for blocked vs non-blocked cholesky
-    if m.nv < 32:
+    if m.nv <= 32:
       wp.launch_tiled(
         update_gradient_cholesky(m.nv),
         dim=d.nworld,
