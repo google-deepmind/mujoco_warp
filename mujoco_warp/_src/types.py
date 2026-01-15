c3f7b68bfb4ba1776536f35126efeb0ed1622925
Alain Denzler <adenzler@nvidia.com>
2026-01-15 11:43:40 +0100

    remove comment again
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>

--- mujoco_warp/_src/types.py
+++ mujoco_warp/_src/types.py
@@ -1505,7 +1505,7 @@ class Constraint:
     state: constraint state                           (nworld, njmax_pad)
     mv: qM @ search                                   (nworld, nv)
     jv: efc_J @ search                                (nworld, njmax)
-    quad: quadratic cost coefficients                 (nworld, njmax, 3)  # parallel, or iterative+elliptic
+    quad: quadratic cost coefficients                 (nworld, njmax, 3)
     quad_gauss: quadratic cost Gauss coefficients     (nworld, 3)  # parallel linesearch only
     alpha: line search step size                      (nworld,)  # parallel linesearch only
     prev_grad: previous grad                          (nworld, nv)
