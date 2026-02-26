15e017e20b55e5b0f02fb523f7a963ecef961fc4
Taylor Howell <taylorhowell@google.com>
2026-02-26 10:37:26 +0000

    add guard for island discovery

--- mujoco_warp/_src/forward.py
+++ mujoco_warp/_src/forward.py
@@ -518,7 +518,8 @@ def fwd_position(m: Model, d: Data, factorize: bool = True):
   if m.opt.run_collision_detection:
     collision_driver.collision(m, d)
   constraint.make_constraint(m, d)
-  if not (m.opt.disableflags & DisableBit.ISLAND):
+  # TODO(team): remove False after island features are more complete 
+  if False and not (m.opt.disableflags & DisableBit.ISLAND):
     island.island(m, d)
   smooth.transmission(m, d)
 
